af48719b4a173cbe8276b99d65f55af132d3dd3f
Hannah Greene <heymanniceshot@yahoo.com>
2022-12-13 18:53:45 -0600

    fixed navbar and link

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import "./Navbar.css"
 
 export const NavBar = () => {
@@ -7,21 +7,21 @@ export const NavBar = () => {
     return (
         <ul className="navbar">
             <li className="navbar__item active">
-                <link className="navbar__link" to="/poems">Home</link>
+                <Link className="navbar__link" to="/poems">Home</Link>
             </li>
             <li className="navbar__item active">
-                <link className="navbar__link" to="/about">About</link>
+                <Link className="navbar__link" to="/about">About</Link>
             </li>
             <li className="navbar__item active">
-                <link className="navbar__link" to="submitpoems">Submit Poems</link>
+                <Link className="navbar__link" to="submitpoems">Submit Poems</Link>
             </li>
             {
                 localStorage.getItem("capstone_user")
                 ? <li className="navbar__item navbar__logout">
-                    <link className="navbar__link" to="" onClick={() => {
+                    <Link className="navbar__link" to="" onClick={() => {
                         localStorage.removeItem("capstone_user")
                         navigate("/", {replace: true})
-                    }}>Logout</link>
+                    }}>Logout</Link>
                 </li>
                 :""
             }
